2fde273d27cc1d29964825e1cc6fb428dbf41634
rmaciasa6 <rmaciasa6@unemi.edu.ec>
2025-09-27 18:23:47 -0500

    eliminando archivo

--- ARCHIVO.py
+++ ARCHIVO.py
@@ -21,13 +21,3 @@ CREATE TABLE dept
 dept_name varchar2(50) NOT NULL,
 CONSTRAINT dept_pk PRIMARY KEY (dept_id)
 );
-
-CREATE TABLE emp
-( emp_no number(10) NOT NULL,
-emp_name varchar2(50) NOT NULL,
-dept_id number(10),
-salary number(6),
-CONSTRAINT emp_pk PRIMARY KEY (emp_no),
-CONSTRAINT dept_fk
-FOREIGN KEY (dept_id)
-REFERENCES dept(dept_id) );
